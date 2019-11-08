3f927f884b25154bf1dd34bca7d4ae1d3569b73b
Ryan Weaver <ryan@thatsquality.com>
2019-11-08 11:34:21 -0500

    tuts hero working...

--- src/Command/UpdateSightingScoresCommand.php
+++ src/Command/UpdateSightingScoresCommand.php
@@ -46,8 +46,8 @@ class UpdateSightingScoresCommand extends Command
 
             $score = ceil(min($characterCount / 500, 10));
             $sighting->setScore($score);
-            $this->entityManager->flush();
         }
+        $this->entityManager->flush();
         $io->progressFinish();
     }
 }
