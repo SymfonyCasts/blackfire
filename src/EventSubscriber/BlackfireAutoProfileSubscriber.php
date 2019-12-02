ced555a726cbc3d1f11e50ada34891b65979091b
Ryan Weaver <ryan@thatsquality.com>
2019-12-02 15:11:29 -0500

    tuts hero working...

--- src/EventSubscriber/BlackfireAutoProfileSubscriber.php
+++ src/EventSubscriber/BlackfireAutoProfileSubscriber.php
@@ -2,6 +2,7 @@
 
 namespace App\EventSubscriber;
 
+use Blackfire\Client;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
 
@@ -9,7 +10,14 @@ class BlackfireAutoProfileSubscriber implements EventSubscriberInterface
 {
     public function onRequestEvent(RequestEvent $event)
     {
-        // ...
+        // replace with some conditional logic
+        $request = $event->getRequest();
+        $shouldProfile = $request->getPathInfo() === '/api/github-organization';
+
+        if ($shouldProfile) {
+            $blackfire = new Client();
+            $probe = $blackfire->createProbe();
+        }
     }
 
     public static function getSubscribedEvents()
