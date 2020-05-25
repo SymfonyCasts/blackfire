589c62f928f092de9a5bc4482eb6a4424ecfc334
Ryan Weaver <ryan@thatsquality.com>
2020-05-25 10:06:22 -0400

    Step 3: First Profile: Property caching

--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -12,6 +12,8 @@ class AppExtension extends AbstractExtension
 {
     private $commentHelper;
 
+    private $userStatuses = [];
+
     public function __construct(CommentHelper $commentHelper)
     {
         $this->commentHelper = $commentHelper;
@@ -25,6 +27,15 @@ class AppExtension extends AbstractExtension
     }
 
     public function getUserActivityText(User $user): string
+    {
+        if (!isset($this->userStatuses[$user->getId()])) {
+            $this->userStatuses[$user->getId()] = $this->calculateUserActivityText($user);
+        }
+
+        return $this->userStatuses[$user->getId()];
+    }
+
+    private function calculateUserActivityText(User $user): string
     {
         $commentCount = $this->commentHelper->countRecentCommentsForUser($user);
 
