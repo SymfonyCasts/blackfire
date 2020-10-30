040458d2c5695e6c1762f17f72feef7df352d8ea
Ryan Weaver <ryan@thatsquality.com>
2020-10-30 10:58:14 -0400

    Step 26: Test: add test assertion

--- tests/Controller/MainControllerTest.php
+++ tests/Controller/MainControllerTest.php
@@ -2,17 +2,27 @@
 
 namespace App\Tests\Controller;
 
+use Blackfire\Bridge\PhpUnit\TestCaseTrait;
+use Blackfire\Profile\Configuration;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class MainControllerTest extends WebTestCase
 {
+    use TestCaseTrait;
+
     public function testGetGitHubOrganization()
     {
         $client = static::createClient();
 
-        $client->request('GET', '/api/github-organization');
-        $this->assertResponseIsSuccessful();
-        $data = json_decode($client->getResponse()->getContent(), true);
-        $this->assertArrayHasKey('organization', $data);
+        $blackfireConfig = (new Configuration())
+            ->assert('metrics.http.requests.count == 1');
+
+        $this->assertBlackfire($blackfireConfig, function() use ($client) {
+            $client->request('GET', '/api/github-organization');
+
+            $this->assertResponseIsSuccessful();
+            $data = json_decode($client->getResponse()->getContent(), true);
+            $this->assertArrayHasKey('organization', $data);
+        });
     }
 }
